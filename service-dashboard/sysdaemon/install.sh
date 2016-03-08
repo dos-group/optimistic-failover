ce533d8360faf1422231d35d97b8868ca49335f1
Tim Jungnickel <tim.jungnickel@tu-berlin.de>
2016-03-08 14:57:12 +0100

    fixed wrong path

--- service-dashboard/sysdaemon/install.sh
+++ service-dashboard/sysdaemon/install.sh
@@ -3,6 +3,6 @@
 cd /opt
 apt-get --assume-yes --force-yes install git python3-psutil python3-requests -f
 git clone https://github.com/citlab/optimistic-failover.git
-cp service-dashboard/sysdaemon/sysdaemon.conf /etc/init/
+cp optimistic-failover/service-dashboard/sysdaemon/sysdaemon.conf /etc/init/
 initctl reload-configuration
 initctl start sysdaemon
